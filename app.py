1f5a8354628650fc39fbfa5c4da1ccb3b902c3e8
Your Name <your@email.com>
2025-04-30 18:35:09 -0700

    <0001f9fc> Force update

--- app.py
+++ app.py
@@ -115,7 +115,6 @@ elif page == "Scorecard Dashboard":
     grouped = grouped[
         (grouped['Recruiter'] == selected_recruiter) &
         (grouped['Department'].isin(selected_depts))
-    ]
 
     if toggle_status == "Complete Scorecards":
         grouped = grouped[grouped['Scorecards_Submitted'] == 4]
@@ -194,9 +193,7 @@ elif page == "📊 Department Analytics":
     submission_rate_df['% Scorecards Submitted'] = ((submission_rate_df['submitted'] / submission_rate_df['total_assigned']) * 100).round().astype(int).astype(str) + '%'
 
     internal_df = pd.merge(internal_df, submission_rate_df[['Internal Interviewer', '% Scorecards Submitted']], on='Internal Interviewer', how='left')
-]
 
-    submission_rate_df = internal_df.groupby('Internal Interviewer').agg(
         total_assigned=('Submitted', 'count'),
         submitted=('Submitted', 'sum')
     ).reset_index()
@@ -205,7 +202,6 @@ elif page == "📊 Department Analytics":
     internal_df = pd.merge(internal_df, submission_rate_df[['Internal Interviewer', '% Scorecards Submitted']], on='Internal Interviewer', how='left')
 
 
-    submission_rate_df = internal_df.groupby('Internal Interviewer').agg(
         total_assigned=('Submitted', 'count'),
         submitted=('Submitted', 'sum')
     ).reset_index()
